0e17f1a49f028f0f3e0a4a458105935a049995be
Harvar3105 <106886851+Harvar3105@users.noreply.github.com>
2025-10-12 15:07:15 +0300

    Delete workers/dist directory
    bruh(

--- workers/dist/utils/types/enums/eCurrency.js
+++
@@ -1,5 +0,0 @@
-export var ECurrency;
-(function (ECurrency) {
-    ECurrency["USD"] = "USD";
-    ECurrency["EUR"] = "EUR";
-})(ECurrency || (ECurrency = {}));
--- workers/dist/utils/types/enums/eVendor.js
+++
@@ -1,6 +0,0 @@
-export var eVendor;
-(function (eVendor) {
-    eVendor["Steam"] = "Steam";
-    eVendor["InstatnGaming"] = "Instant Gaming";
-    eVendor["G2A"] = "G2A";
-})(eVendor || (eVendor = {}));
